dc00a55862c3a87950fd2bd7bd3474b80810b472
Nathan Robbert <admin@bigrocketgames.com>
2017-02-03 12:10:09 -0800

    Passing all model specs.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,25 @@
 class Cart < ActiveRecord::Base
+  belongs_to :user
+  has_many :line_items
+  has_many :items, through: :line_items
+
+  def total
+    total = 0
+    line_items.each do |li|
+      total += (li.quantity * li.item.price)
+    end
+    total
+  end
+
+  def add_item(itemid)
+    line_item = self.line_items.find_by(item_id: itemid)
+
+    if line_item
+      line_item.quantity += 1
+    else
+      line_item = self.line_items.build(item_id: itemid)
+    end
+    line_item
+  end
 
 end
